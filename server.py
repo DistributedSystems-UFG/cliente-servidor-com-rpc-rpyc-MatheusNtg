5996c6b5ff3948cc10645b7b14b2a97781382cec
Matheus Rodrigues <matheussr61@gmail.com>
2022-03-29 19:50:22 -0300

    add basic logs to the server

--- server.py
+++ server.py
@@ -4,15 +4,19 @@ from rpyc.utils.server import ThreadedServer
 
 class Calculator(rpyc.Service):
 	def exposed_sum(self, op1, op2):
+		print(f"received sum operation with operands {op1} and {op2}")
 		return op1 + op2
 
 	def exposed_mult(self, op1, op2):
+		print(f"received mult operation with operands {op1} and {op2}")
 		return op1 * op2
 
 	def exposed_div(self, op1, op2):
+		print(f"received div operation with operands {op1} and {op2}")
 		return op1 / op2
 
 	def exposed_sub(self, op1, op2):
+		print(f"received sub operation with operands {op1} and {op2}")
 		return op1 - op2
 
 if __name__ == "__main__":
